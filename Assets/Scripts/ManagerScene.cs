8693bd1b5554ef18e3d60fd5accd7fc996fbce7d
Fatima Morales <fatima.rmb13@gmail.com>
2026-07-29 14:32:50 -0700

    Created HubWorld
    Created a basic HubWorld!
    
    Clicking "Start" takes you to the HubWorld, and each Level Button takes you to that Level!!

--- Assets/Scripts/ManagerScene.cs
+++ Assets/Scripts/ManagerScene.cs
@@ -7,19 +7,36 @@ using System.Collections.Generic;
 
 public class ManagerScene : MonoBehaviour
 {
+    public GameObject StartMenu;
+    public GameObject HubWorld;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        HubWorld.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-      public void PlayGame()
+      public void LoadHubWorld()
+    {
+        StartMenu.gameObject.SetActive(false);
+        HubWorld.gameObject.SetActive(true);
+    }
+    public void LevelOne()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+        public void LevelTwo()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+        public void LevelThree()
+    {
+        SceneManager.LoadSceneAsync(3);
     }
 }
