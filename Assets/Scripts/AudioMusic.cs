e7dab5701472b920f4ac5b696b5ccb10f269e253
Fatima Morales <fatima.rmb13@gmail.com>
2026-09-12 14:25:09 -0700

    Scaling, Buttons, Music
    Fixed the scaling problem for the main menu and HubWorld! I also lowkey changed the slider but it looks butt.
    
    I also added the player to the beginning of level 1!
    
    I ALSO added the level four music, and had it play in the main menu and hubworld

--- Assets/Scripts/AudioMusic.cs
+++ Assets/Scripts/AudioMusic.cs
@@ -7,11 +7,15 @@ public class AudioMusic : MonoBehaviour
     public AudioClip background_one;
     public AudioClip background_two;
     public AudioClip background_three;
+    public AudioClip background_four;
+
 
 
     public bool LevelOne;
     public bool LevelTwo;
     public bool LevelThree;
+    public bool LevelFour;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,6 +32,10 @@ public class AudioMusic : MonoBehaviour
         else if(LevelThree)
         {
         musicSource.clip = background_three;
+        }
+        else if(LevelFour)
+        {
+        musicSource.clip = background_four;
         }
         musicSource.Play();
     }
